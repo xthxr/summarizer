f2e45262b6ace90bc5340fb6e87f40a577226727
Athar <129141365+xthxr@users.noreply.github.com>
2025-05-20 03:04:58 +0530

    Update fetchWebContent.js

--- backend/utils/fetchWebContent.js
+++ backend/utils/fetchWebContent.js
@@ -1,1 +1,12 @@
 
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const fetchWebContent = async (url) => {
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const text = $("body").text();
+  return text.replace(/\s+/g, " ").trim();
+};
+
+module.exports = fetchWebContent;
